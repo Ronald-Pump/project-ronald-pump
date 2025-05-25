9fcc9a94eb443857882a235b7d1ebce96a589dde
Haziq Noor <haziq.noor@outlook.com>
2025-05-25 22:20:28 +0800

    Adding Lambda Resource

--- 00_infrastructure/main.tf
+++ 00_infrastructure/main.tf
@@ -2,14 +2,11 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
-  acl    = "private"
 
-  # Enable versioning for the bucket
-  versioning {
-    enabled = true
-  }
+###################################################### For the S3 bucket structure ######################################################
+resource "aws_s3_bucket" "my_bucket" {
+  bucket      = var.bucket_name
+  acl         = "private"
 
   tags = {
     Name        = var.bucket_name
@@ -17,23 +14,64 @@ resource "aws_s3_bucket" "my_bucket" {
   }
 }
 
-# Create the initial folder inside the bucket
-resource "aws_s3_object" "initial_folder" {
-  bucket = aws_s3_bucket.my_bucket.bucket
-  key    = "initial-pdf-dump/"  # The trailing slash indicates a folder
-  acl    = "private"
+
+# Create the initial-pdf-dump folder structure
+resource "aws_s3_object" "initial_pdf_dump_folders" {
+  for_each = toset([
+    "initial-pdf-dump/",
+    "initial-pdf-dump/haziq/",
+    "initial-pdf-dump/teh/"
+  ])
+
+  bucket  = aws_s3_bucket.my_bucket.bucket
+  key     = each.value
+  content = "" # Empty content to create folder structure
+  acl     = "private"
 }
 
-# Create the 'haziq' subfolder inside 'initial-pdf-dump'
-resource "aws_s3_object" "haziq_folder" {
-  bucket = aws_s3_bucket.my_bucket.bucket
-  key    = "initial-pdf-dump/haziq/"  # The trailing slash indicates a folder
-  acl    = "private"
+# Create the evolt-scan folder structure
+resource "aws_s3_object" "evolt_scan_folders" {
+  for_each = toset([
+    "evolt-scan/",
+    "evolt-scan/input/",
+    "evolt-scan/output/",
+    "evolt-scan/input/haziq/",
+    "evolt-scan/input/teh/",
+    "evolt-scan/output/haziq/",
+    "evolt-scan/output/teh/"
+  ])
+
+  bucket  = aws_s3_bucket.my_bucket.bucket
+  key     = each.value
+  content = "" # Empty content to create folder structure
+  acl     = "private"
 }
 
-# Create the 'teh' subfolder inside 'initial-pdf-dump'
-resource "aws_s3_object" "teh_folder" {
-  bucket = aws_s3_bucket.my_bucket.bucket
-  key    = "initial-pdf-dump/teh/"  # The trailing slash indicates a folder
-  acl    = "private"
+###################################################### For the Lambda structure ######################################################
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda-exec-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_function" "bia_extractor" {
+  function_name    = "bia-extractor"
+  runtime          = "python3.11"
+  handler          = "lambda_function.lambda_handler" # Matches the file and function name in your Lambda code
+  role             = aws_iam_role.lambda_exec_role.arn
+  filename         = "${path.module}/../01_lambda/lambda_package.zip" # Points to the zip file in the 01_lambda folder
+  source_code_hash = filebase64sha256("${path.module}/../01_lambda/lambda_package.zip") # Ensures Terraform detects changes
+  timeout          = 60
+  memory_size      = 512
 }
--- 00_infrastructure/outputs.tf
+++ 00_infrastructure/outputs.tf
@@ -1,3 +1,4 @@
+###################################################### For the S3 bucket structure ######################################################
 output "s3_bucket_id" {
   value = aws_s3_bucket.my_bucket.id
 }
@@ -8,4 +9,16 @@ output "s3_bucket_arn" {
 
 output "s3_bucket_url" {
   value = aws_s3_bucket.my_bucket.website_endpoint
+}
+
+
+###################################################### For the Lambda structure ######################################################
+output "lambda_function_arn" {
+  description = "The ARN of the Lambda function"
+  value       = aws_lambda_function.bia_extractor.arn
+}
+
+output "lambda_function_name" {
+  description = "The name of the Lambda function"
+  value       = aws_lambda_function.bia_extractor.function_name
 }
